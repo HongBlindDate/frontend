1c818fc11118cb4389342b43629a4408e7b3641e
seongbin <dannybin814@naver.com>
2024-02-19 11:13:00 +0900

    first commit

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import BottomNavigation from './BottomNavigation';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+//App의 시작, login이 되어있다면 메인화면, 로그인이 안되어있다면 로그인화면
+
+//login이 되어있을 경우, 메인페이지(매칭)와 하단바
+export default function App() {
+  return (
+    //to commit 
+
+    <NavigationContainer>
+      <BottomNavigation />
+    </NavigationContainer> 
+    
+  );
+}
---
+++ frontend/screen/profile.js
@@ -0,0 +1,15 @@
+//profile 기본화면
+import react from "react";
+import { Text, View } from 'react-native';
+
+
+const Profile = () => {
+
+    return (
+    <View>
+        <Text> profile page!</Text>
+    </View>
+    );
+}
+
+export default Profile;
